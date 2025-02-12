2445189da80883e38b71989d555ab270034ccb58
gianl <gianluigilopardo@gmail.com>
2025-02-12 23:56:51 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,85 @@
+import streamlit as st
+
+from dotenv import load_dotenv 
+
+from langchain_community.document_loaders import PyPDFLoader 
+from langchain.chains import RetrievalQA 
+from langchain_community.vectorstores import FAISS 
+
+from langchain_openai.embeddings import OpenAIEmbeddings 
+# from langchain_nvidia_ai_endpoints.embeddings import NVIDIAEmbeddings
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter 
+
+# from langchain_openai import ChatOpenAI 
+from langchain_nvidia_ai_endpoints import ChatNVIDIA
+
+
+load_dotenv() 
+
+# Set the title of the Streamlit app
+st.title(f"Hack the Act! 🤖")
+# Add a markdown description of the app
+st.markdown(
+    """
+        **Hack the Act!** is a RAG-powered chatbot designed to demystify the [European Union AI Act](https://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=OJ:L_202401689). 
+        The underlying LLM is [Colosseum 355B](https://build.nvidia.com/igenius/colosseum_355b_instruct_16k) by [iGenius](https://www.igenius.ai/), a model tailored for regulated industries. 
+"""
+#        Available in five languages: English, Italian, French, German, and Spanish. 
+)
+
+# Define a function to set up the question answering system
+def setup_qa_system(dir, files): 
+    # Load the relevant PDFs
+    docs = [doc for file in files for doc in PyPDFLoader(f'{dir}{file}.pdf').load_and_split()]
+
+    # Split the documents into smaller chunks for processing
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_documents(docs)
+
+    # Create embeddings for the document chunks
+    # embeddings = NVIDIAEmbeddings()
+    embeddings = OpenAIEmbeddings()
+    vector_store = FAISS.from_documents(chunks, embeddings)
+
+    # Create a retriever from the vector store
+    retriever = vector_store.as_retriever()
+    # Initialize the ChatNVIDIA model
+    llm = ChatNVIDIA(
+            model="igenius/colosseum_355b_instruct_16k",
+            temperature=0.1,
+            top_p=0.7,
+            max_tokens=1024,
+    )
+
+    # Create a RetrievalQA chain
+    qa_chain = RetrievalQA.from_chain_type(llm, retriever=retriever)
+
+    return qa_chain 
+
+# Define the directory and file names for the documents
+dir = "https://raw.githubusercontent.com/gianluigilopardo/hacktheact/main/docs/"
+files = ['act_en', #'act_it', 'act_es', 'act_fr', 'act_de', 
+        'annex_en', #'annex_it', 'annex_es', 'annex_fr', 'annex_de', 
+    ]
+
+# Set up the question answering system
+qa_chain = setup_qa_system(dir, files)
+
+# Define a function to generate a response to a question
+def generate_response(question):
+    answer = qa_chain.invoke(question)
+    st.info(answer['result'])
+   
+# Create a Streamlit form
+with st.form("aiact_form"):
+    # Add a text area for the user to enter their question
+    text = st.text_area(
+        "Ask any question about the EU AI Act:", 
+        "What are the basic principles of the AI Act? "
+        )
+    
+    # Add a submit button
+    submitted = st.form_submit_button("Submit")
+    # Generate and display the response if the form is submitted
+    generate_response(text)
